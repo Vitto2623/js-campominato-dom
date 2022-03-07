ffc78472e21254652a56362d40376c1e47b1cb59
Vitto2623 <vtr.1@icloud.com>
2022-03-07 16:49:33 +0100

    aggiungo contatore punteggio dopo griglia numeri

---
+++ js/main.js
@@ -0,0 +1,61 @@
+// recupero la griglia
+const gridElement = document.getElementById("grid");
+
+const sceltaPlayer = document.getElementById("difficolta");
+let numeroCell;
+let dimensioniCell;
+const buttonElement = document.getElementById('btn');
+buttonElement.addEventListener('click', function(){
+    gridElement.innerHTML = "";
+    if(sceltaPlayer.value == "facile"){
+        numeroCell = 100;
+        dimensioniCell = "square-1";
+    } else if(sceltaPlayer.value == "medio"){
+        numeroCell = 81;
+        dimensioniCell = "square-2";
+    }   else{
+        numeroCell = 49;
+        dimensioniCell = "square-3";
+    }
+
+    //genera 16 numeri casuali per le bombe
+    const numeroBombe = [];
+    let bombaProvvisoria;
+    for ( let j = 1; j <= 16; j++){
+        bomba = (randomNumber(1, numeroCell));
+        for (let t = 1; t <= j; t++){
+            if (bombaProvvisoria == numeroBombe){
+                bombaProvvisoria = (randomNumber(1, numeroCell))
+            }
+        }
+        numeroBombe.push(bomba);
+    };
+
+    console.log(numeroBombe);
+
+    for (let i = 1; i < (numeroCell+1); i++){
+        const square = document.createElement("div");
+        square.classList.add(dimensioniCell);
+        gridElement.appendChild(square);
+        square.innerHTML = i;
+        square.addEventListener('click', function(){
+            if (!numeroBombe.includes(i) ){
+                square.classList.add('cliccato');
+            }else {
+                square.classList.add('bomba');
+            }
+        });
+        
+    }
+
+
+});
+
+// la funzione restituisce un numero random fra un valore minimo e uno massimo
+function randomNumber(minimumValue, maximumValue){
+    //outPutNumeroPc.innerHTML += Math.floor(Math.random()*((maximumValue + 1) - minimumValue) + minimumValue);
+    let valorePc = Math.floor(Math.random()*((maximumValue + 1) - minimumValue) + minimumValue);
+    return valorePc;
+}
+
+//creo una funziona che 
